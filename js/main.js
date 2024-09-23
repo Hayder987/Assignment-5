e03096d725c29b1f9a07067c8bb22028bc26040e
hayder <hayderbd4290@gmail.com>
2024-09-23 11:36:46 +0600

    modal and all card calculation done

--- js/main.js
+++ js/main.js
@@ -11,3 +11,74 @@ document.getElementById("historyBtn").addEventListener("click", function(){
     displayDiv("historySection", "historyBtn");
     btnManipulation("historyBtn");
 });
+
+document.getElementById("card1btn").addEventListener("click", function(){
+
+    let inputValue1 = getInputValue("card1Input");
+    let TotalAmount = getTextValue ("totalAmount");
+    let currentDonation1 = getTextValue ("currentDonation1-text");
+    if(isNaN(inputValue1) || TotalAmount<inputValue1 || inputValue1<0){
+        alert("Wrong Input Please Enter Correct Value");
+        return ;
+    }
+    else{
+       
+        dataCalculation (inputValue1, TotalAmount, currentDonation1, "currentDonation1-text" ) ;
+        document.getElementById("modal1").setAttribute("href", "#my_modal_8");
+
+        document.getElementById("modal1-p-text").innerText = `You Have Donated $ ${inputValue1} for Humankind `
+        
+        document.getElementById("confirmBtn1").addEventListener("click", function(){
+            document.getElementById("modal1").removeAttribute("href"); 
+        })
+    }
+      
+});
+
+
+document.getElementById("card2btn").addEventListener("click", function(){
+
+    let inputValue2 = getInputValue("card2Input");
+    let TotalAmount = getTextValue ("totalAmount");
+    let currentDonation2 = getTextValue ("currentDonation2-text");
+    if(isNaN(inputValue2) || TotalAmount<inputValue2 || inputValue2<0){
+        alert("Wrong Input Please Enter Correct Value");
+        return ;
+    }
+    else{
+       
+        dataCalculation (inputValue2, TotalAmount, currentDonation2, "currentDonation2-text" ) ;
+        document.getElementById("modal2").setAttribute("href", "#my_modal_8");
+
+        document.getElementById("modal1-p-text").innerText = `You Have Donated $ ${inputValue2} for Humankind `
+        
+        document.getElementById("confirmBtn1").addEventListener("click", function(){
+            document.getElementById("modal2").removeAttribute("href"); 
+        })
+    }
+      
+});
+
+
+document.getElementById("card3btn").addEventListener("click", function(){
+
+    let inputValue3 = getInputValue("card3Input");
+    let TotalAmount = getTextValue ("totalAmount");
+    let currentDonation3 = getTextValue ("currentDonation3-text");
+    if(isNaN(inputValue3) || TotalAmount<inputValue3 || inputValue3<0){
+        alert("Wrong Input Please Enter Correct Value");
+        return ;
+    }
+    else{
+       
+        dataCalculation (inputValue3, TotalAmount, currentDonation3, "currentDonation3-text" ) ;
+        document.getElementById("modal3").setAttribute("href", "#my_modal_8");
+
+        document.getElementById("modal1-p-text").innerText = `You Have Donated $ ${inputValue3} for Humankind `
+        
+        document.getElementById("confirmBtn1").addEventListener("click", function(){
+            document.getElementById("modal3").removeAttribute("href"); 
+        })
+    }
+      
+});
--- js/utility.js
+++ js/utility.js
@@ -1,14 +1,9 @@
 
-function displayDiv(id, btnId){
+function displayDiv(id){
   document.getElementById("donationSection").classList.add("hidden");
-
   document.getElementById("historySection").classList.add("hidden");
 
-
-  document.getElementById(id).classList.remove("hidden");
-  
-  
-  
+  document.getElementById(id).classList.remove("hidden"); 
 };
 
 function btnManipulation(id){
@@ -22,4 +17,35 @@ function btnManipulation(id){
     let selectedBtn = document.getElementById(id); 
     selectedBtn.classList.add("bg-btn-bg");
     selectedBtn.classList.remove("border-2", "border-gray-400")
-}
+};
+
+
+function getInputValue (id){
+     let getInputValue = document.getElementById(id);
+     let   inputValue = getInputValue.value
+     let getInputValueNumber = parseFloat(inputValue); 
+     getInputValue.value ='';
+
+     return getInputValueNumber;
+};
+
+
+function getTextValue (id){
+  let TextValue = document.getElementById(id).innerText;
+  let TextValueNumber = parseFloat(TextValue);
+
+  return TextValueNumber;
+};
+
+function dataCalculation (input, totalAmount, donationAmount, eachDonate){
+
+   let restTotal = totalAmount - input;
+
+   let totalDonation = donationAmount + input;
+
+   document.getElementById("totalAmount").innerText = restTotal;
+   document.getElementById(eachDonate).innerHTML = totalDonation;
+
+
+
+};
